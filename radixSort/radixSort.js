a0d7a88c4af6be14e106ffdc7865c6f78dd4faea
David Becker <david@beckersite.com>
2018-02-12 14:12:57 -0800

    Pseudo code for processes that still need figuring out

--- radixSort/radixSort.js
+++ radixSort/radixSort.js
@@ -2,18 +2,30 @@
 
 // first pass
 
+[211, 81, 22, 203, 13, 44, 5, 726, 9]
+
+// second pass
+
+[203, 5, 9, 211, 13, 22, 726, 44, 81]
+
+// third pass
+
+[5, 9, 13, 22, 44, 81, 203, 211, 726]
+
+// sorted once process sorts largest digits place
+
 const radixSort = (array) => {
 	const buckets = {
 		0: [],
-		1: [211, 81],
-		2: [22],
-		3: [203, 13],
-		4: [44],
-		5: [5],
-		6: [726],
+		1: [],
+		2: [],
+		3: [],
+		4: [],
+		5: [],
+		6: [],
 		7: [],
 		8: [],
-		9: [9]
+		9: []
 	}
 
 	const largestNumDigits = findLargestNumber(array).length
@@ -23,8 +35,9 @@ const radixSort = (array) => {
 const findLargestNumber = (array) => {
 	return Math.max(...array)
 }
+// need to figure out how to determine which digits place currently on
 
-
+// need to figure out how to determine when to stop recursive process (if only #s of 3 digits, no need to check 4th digit in list)
 
 // unload buckets, create new array
 
