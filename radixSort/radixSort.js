6865a484c1195f8e93daff663d612aeb165c90ff
David Becker <david@beckersite.com>
2018-02-12 14:05:02 -0800

    Add function for unloading buckets at each pass and creating new array

--- radixSort/radixSort.js
+++ radixSort/radixSort.js
@@ -1,15 +1,19 @@
+[203, 44, 211, 22, 13, 5, 726, 9, 81]
+
+// first pass
+
 const radixSort = (array) => {
 	const buckets = {
 		0: [],
-		1: [],
-		2: [],
-		3: [],
-		4: [],
-		5: [],
-		6: [],
+		1: [211, 81],
+		2: [22],
+		3: [203, 13],
+		4: [44],
+		5: [5],
+		6: [726],
 		7: [],
 		8: [],
-		9: []
+		9: [9]
 	}
 
 	const largestNumDigits = findLargestNumber(array).length
@@ -18,4 +22,25 @@ const radixSort = (array) => {
 
 const findLargestNumber = (array) => {
 	return Math.max(...array)
+}
+
+
+
+// unload buckets, create new array
+
+const unloadBucketsReset = (buckets) => {
+  const newArray = []
+  // for loop 10 times, checking size of buckets
+  for (let i = 0; i < 10; i++) {
+    if (!buckets[i].length) {
+      continue;
+    }
+
+    for (let j = 0; j < buckets[i].length; j++) {
+      newArray.push(buckets[i][j])
+    }
+  }
+    // if bucket is empty, continue
+    // unload bucket into newArray => FIFO process (push into array)
+  return newArray
 }
