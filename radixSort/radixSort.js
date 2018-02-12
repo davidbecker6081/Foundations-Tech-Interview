614f9fe4c3040236b29678dd10a7ee2f86e203e1
David Becker <david@beckersite.com>
2018-02-12 14:27:31 -0800

    Add logic inside radix function

--- radixSort/radixSort.js
+++ radixSort/radixSort.js
@@ -14,21 +14,37 @@
 
 // sorted once process sorts largest digits place
 
-const radixSort = (array) => {
-	const buckets = {
-		0: [],
-		1: [],
-		2: [],
-		3: [],
-		4: [],
-		5: [],
-		6: [],
-		7: [],
-		8: [],
-		9: []
-	}
+const radixSort = (array, counter = 0) => {
+  let largestNumDigits;
+  let counter = counter;
 
-	const largestNumDigits = findLargestNumber(array).length
+  if (counter === 0) {
+    largestNumDigits = findLargestNumber(array).length
+  }
+
+  if (counter !== largestNumDigits) {
+
+  	const buckets = {
+  		0: [],
+  		1: [],
+  		2: [],
+  		3: [],
+  		4: [],
+  		5: [],
+  		6: [],
+  		7: [],
+  		8: [],
+  		9: []
+  	}
+
+    // function to load buckets at index counter
+
+    // unloadBucketsReset
+    // store new array
+    return radixSort(newArray, counter++)
+  } else {
+    return array
+  }
 
 }
 
