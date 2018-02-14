8e2df4c1400c43e272dea031bd9017b15a484feb
David Becker <david@beckersite.com>
2018-02-14 14:51:46 -0700

    Fix infinite loop in radixSort function

--- radixSort/radixSort.js
+++ radixSort/radixSort.js
@@ -1,28 +1,47 @@
-[203, 44, 211, 22, 13, 5, 726, 9, 81]
+const toSort = [203, 44, 211, 22, 13, 5, 726, 9, 81];
+
+const findLargestNumber = (array) => {
+	const largestNum = Math.max(...array)
+	return largestNum.toString().length
+}
+
+const unloadBucketsReset = (buckets) => {
+  const newArray = []
+  // for loop 10 times, checking size of buckets
+  for (let i = 0; i < 10; i++) {
+    if (!buckets[i].length) {
+      continue;
+    }
+
+    for (let j = 0; j < buckets[i].length; j++) {
+      newArray.push(buckets[i][j])
+    }
+  }
+    // if bucket is empty, continue
+    // unload bucket into newArray => FIFO process (push into array)
+  return newArray
+}
 
 // first pass
 
-[211, 81, 22, 203, 13, 44, 5, 726, 9]
+// [211, 81, 22, 203, 13, 44, 5, 726, 9]
 
-// second pass
+// // second pass
 
-[203, 5, 9, 211, 13, 22, 726, 44, 81]
+// [203, 5, 9, 211, 13, 22, 726, 44, 81]
 
-// third pass
+// // third pass
 
-[5, 9, 13, 22, 44, 81, 203, 211, 726]
+// [5, 9, 13, 22, 44, 81, 203, 211, 726]
 
 // sorted once process sorts largest digits place
 
-const radixSort = (array, counter = 0) => {
-  let largestNumDigits;
-  let counter = counter;
-
-  if (counter === 0) {
-    largestNumDigits = findLargestNumber(array).length
-  }
+const radixSort = (array, counter) => {
+  let largestNumDigits = findLargestNumber(array)
+  console.log(largestNumDigits)
 
   if (counter !== largestNumDigits) {
+  	console.log('counter', counter)
 
   	const buckets = {
   		0: [],
@@ -41,35 +60,17 @@ const radixSort = (array, counter = 0) => {
 
     // unloadBucketsReset
     // store new array
-    return radixSort(newArray, counter++)
+    counter += 1
+    return radixSort(array, counter)
   } else {
-    return array
+  	return array
   }
-
 }
 
-const findLargestNumber = (array) => {
-	return Math.max(...array)
-}
 // need to figure out how to determine which digits place currently on
 
 // need to figure out how to determine when to stop recursive process (if only #s of 3 digits, no need to check 4th digit in list)
 
 // unload buckets, create new array
 
-const unloadBucketsReset = (buckets) => {
-  const newArray = []
-  // for loop 10 times, checking size of buckets
-  for (let i = 0; i < 10; i++) {
-    if (!buckets[i].length) {
-      continue;
-    }
-
-    for (let j = 0; j < buckets[i].length; j++) {
-      newArray.push(buckets[i][j])
-    }
-  }
-    // if bucket is empty, continue
-    // unload bucket into newArray => FIFO process (push into array)
-  return newArray
-}
+radixSort(toSort, 0)
