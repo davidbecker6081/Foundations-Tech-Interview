751565946fee39870b1d6bc98ec6fa8d67480bf6
David Becker <david@beckersite.com>
2018-02-16 18:36:40 -0700

    Slight refactor and renaming of vars and functions

--- radixSort/radixSort.js
+++ radixSort/radixSort.js
@@ -1,36 +1,28 @@
 const toSort = [203, 44, 211, 22, 13, 5, 726, 9, 81];
 
-const findLargestNumber = (array) => {
+const findLargestNumberLength = (array) => {
 	const largestNum = Math.max(...array)
 	return largestNum.toString().length
 }
 
-const convertArrayToString = (array, max) => {
+const convertArrayToString = (array, maxLength) => {
 	return array.map((num) => {
-		num = num.toString()
-		if (num.toString().length < max) {
-			const numOfZeros = max - num.toString().length
-			for (let j = 0; j < numOfZeros; j++) {
+		const numOfZerosToAdd = maxLength - num.toString().length
+			for (let j = 0; j < numOfZerosToAdd; j++) {
 				num = '0' + num
 			}
-		}
-
-		return num
+		return num.toString()
 	})
 }
 
-// convertArrayToString(toSort, 3)
-
-const unloadBucketsReset = (buckets) => {
+const unloadBucketsAndReset = (buckets) => {
   const newArray = []
 
   for (let i = 0; i < 10; i++) {
-    if (!buckets[i.toString()].length) {
-      continue;
-    }
-
-    for (let j = 0; j < buckets[i.toString()].length; j++) {
-      newArray.push(buckets[i.toString()][j])
+    if (buckets[i].length) {
+      for (let j = 0; j < buckets[i].length; j++) {
+      	newArray.push(buckets[i][j])
+  		}
     }
   }
 
@@ -49,33 +41,18 @@ const loadBuckets = (array, digitsPlace) => {
   		'7': [],
   		'8': [],
   		'9': []
-  	}
+  }
+
 	return array.reduce((loadedBuckets, num) => {
 		loadedBuckets[num[digitsPlace]].push(num)
 		return loadedBuckets
 	}, buckets)
 }
 
-const convertArrayToInteger = (array) => {
-	const arrayOfIntegers = array.map(num => parseInt(num))
-	return arrayOfIntegers
-}
+const convertArrayToIntegers = (array) => array.map(num => parseInt(num))
 
-// first pass
-
-// [211, 81, 22, 203, 13, 44, 5, 726, 9]
-
-// // second pass
-
-// [203, 5, 9, 211, 13, 22, 726, 44, 81]
-
-// // third pass
-
-// [5, 9, 13, 22, 44, 81, 203, 211, 726]
-
-
-const radixSort = (array, counter) => {
-  let largestNumDigits = findLargestNumber(array)
+const radixSort = (array, counter = 0) => {
+  let largestNumDigits = findLargestNumberLength(array)
   let stringArray = array
 
   if (counter === 0) {
@@ -83,15 +60,14 @@ const radixSort = (array, counter) => {
   }
 
   if (counter !== largestNumDigits) {
-  	const digitsPlace = largestNumDigits - counter - 1
-    const loadedBuckets = loadBuckets(stringArray, digitsPlace)
-    const newArray = unloadBucketsReset(loadedBuckets)
+  	const currentDigitsPlace = largestNumDigits - counter - 1
+    const loadedBuckets = loadBuckets(stringArray, currentDigitsPlace)
+    const newArray = unloadBucketsAndReset(loadedBuckets)
 
-    counter += 1
-    return radixSort(newArray, counter)
+    return radixSort(newArray, counter += 1)
   } else {
-  	return convertArrayToInteger(stringArray)
+  	return convertArrayToIntegers(stringArray)
   }
 }
 
-radixSort(toSort, 0)
+radixSort(toSort)
